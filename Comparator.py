e451399fab752d99f7ed7755ec491dd6aec637bc
Voligoma <yacussijuancruz@gmail.com>
2024-03-04 19:48:06 -0300

    final (for now)

--- Comparator.py
+++ Comparator.py
@@ -2,20 +2,19 @@ import pandas as pd
 import os
 
 def compare(file1, file2, columna):
-    df_new = pd.read_csv(file1, sep=None)
-    df_old = pd.read_csv(file2, sep=None)
+    df_new = pd.read_csv(file1, sep=";")
+    df_old = pd.read_csv(file2, sep=";")
     new_articles = df_new[columna].isin(df_old[columna])
     new_articles = df_new[~new_articles]
-    print(new_articles)
     try:
-        new_articles.to_csv(r"data/mercadolibre_data_dif.csv", sep=";")
+        new_articles.to_csv(r"data/difference.csv", sep=";", index=False)
     except FileExistsError:
         os.remove("data/mercadolibre_data_odif.csv")
-        new_articles.to_csv(r"data/mercadolibre_data_dif.csv", sep=";")
+        new_articles.to_csv(r"data/difference.csv", sep=";", index=False)
         print("Archivo dif antiguo eliminado")
     except FileNotFoundError:
         print("el archivo no existe creeando.... ") 
-        new_articles.to_csv(r"data/mercadolibre_data_dif.csv", sep=";") 
+        new_articles.to_csv(r"data/difference.csv", sep=";", index=False) 
 
 def merge(file1, file2, file3, file4, file5):
     df1 = pd.read_csv(file1, sep=";")
@@ -27,13 +26,19 @@ def merge(file1, file2, file3, file4, file5):
     frames = [df1, df2, df3, df4, df5]
     result = pd.concat(frames)
     result.to_csv(r"data/out.csv", sep=";", index=False)
+    os.remove(file1)
+    os.remove(file2)
+    os.remove(file3)
+    os.remove(file4)
+    os.remove(file5)
     
 def removeDuplicates (file):
     df = pd.read_csv(file,sep=";")
     df.drop_duplicates(subset=['title'], keep='last', inplace=True, ignore_index=True)
-    df.to_csv(r"data/mercadolibre_data.csv", sep=";", index=False)
+    os.remove(file)
+    df.to_csv(r"data/new.csv", sep=";", index=False)
     
    
-merge("data/1.csv","data/2.csv","data/3.csv","data/4.csv","data/5.csv")
-removeDuplicates("data/out.csv")    
-#compare("data/mercadolibre_data.csv", "data/mercadolibre_data_old.csv", "title")
+#merge("data/mercadolibre_data_1.csv","data/mercadolibre_data_2.csv","data/mercadolibre_data_3.csv","data/mercadolibre_data_4.csv","data/mercadolibre_data_5.csv")
+#removeDuplicates("data/out.csv")   
+compare("data/new.csv", "data/base.csv", "title")
--- main.py
+++ main.py
@@ -6,7 +6,7 @@ import time
 #from Comparator import *
 
 import datetime
-
+Numero = 0
 now = datetime.datetime.now()
 now = now.strftime("%d-%m-%Y-%H-%M")
 print(now)
@@ -69,100 +69,82 @@ class Scraper():
             else:
                 print("Escoge un número del 1 al 18")
 
-
-
     def scraping(self):
-        global Numero
-        Numero = 0
-        while(Numero <= 5):
+        
+        # User search
+        global product_name
+        #product_name = input("\nProducto: ")
+        product_name = "servidor"
+        # Clean the user input
+        cleaned_name = product_name.replace(" ", "-").lower()
+        # Create the urls to scrap
+        urls = [self.base_url + cleaned_name]
+
+        page_number = 50
+
+        for i in range(0, 10000, 50):
+            urls.append(f"{self.base_url}{cleaned_name}_Desde_{page_number + 1}_NoIndex_True")
+            page_number += 50
+        # create a list to save the data
+        self.data = []
+        # create counter
+        c = 1
             
-            Numero = Numero + 1  
-            # User search
-            global product_name
-            #product_name = input("\nProducto: ")
-            product_name = "servidor"
-            # Clean the user input
-            cleaned_name = product_name.replace(" ", "-").lower()
-            # Create the urls to scrap
-            urls = [self.base_url + cleaned_name]
+        # Iterate over each url
+        for i, url in enumerate(urls, start=1):
 
-            page_number = 50
-
-            for i in range(0, 10000, 50):
-                urls.append(f"{self.base_url}{cleaned_name}_Desde_{page_number + 1}_NoIndex_True")
-                page_number += 50
-            # create a list to save the data
-            self.data = []
-            # create counter
-            c = 1
+            # Get the html of the page
+            response = requests.get(url)
+            soup = BeautifulSoup(response.text, 'html.parser')
                 
-            # Iterate over each url
-            for i, url in enumerate(urls, start=1):
+            # take all posts
+            content = soup.find_all('li', class_='ui-search-layout__item')
+            
+            # Check if there's no content to scrape
+            if not content:
+                print("\nTermino el scraping.")
+                break
 
-                # Get the html of the page
-                response = requests.get(url)
-                soup = BeautifulSoup(response.text, 'html.parser')
-                    
-                # take all posts
-                content = soup.find_all('li', class_='ui-search-layout__item')
+            print(f"\nScrapeando pagina numero {i}. {url}")
+            
+            
+            # iteration to scrape posts
+            for post in content:
+                # get the title
+                title = post.find('h2').text
+                # get the price
+                price = post.find('span', class_='andes-money-amount__fraction').text
+                # get the url post
+                post_link = post.find("a")["href"]
+                # get the url image
+               # try:
+                #    img_link = post.find("img")["data-src"]
+                #except:
+                #    img_link = post.find("img")["src"]
                 
-                # Check if there's no content to scrape
-                if not content:
-                    print("\nTermino el scraping.")
-                    break
+                # show the data already scraped
+                # print(f"{c}. {title}, {price}, {post_link}, {img_link}")
 
-                print(f"\nScrapeando pagina numero {i}. {url}")
-                
-                
-                # iteration to scrape posts
-                for post in content:
-                    # get the title
-                    title = post.find('h2').text
-                    # get the price
-                    price = post.find('span', class_='andes-money-amount__fraction').text
-                    # get the url post
-                    post_link = post.find("a")["href"]
-                    # get the url image
-                # try:
-                    #    img_link = post.find("img")["data-src"]
-                    #except:
-                    #    img_link = post.find("img")["src"]
-                    
-                    # show the data already scraped
-                    # print(f"{c}. {title}, {price}, {post_link}, {img_link}")
-
-                    # save in a dictionary
-                    post_data = {
-                        "title": title,
-                        "price": price,
-                        "post link": post_link,
-                    #    "image link": img_link            
-                    }
-                    # save the dictionaries in a list
-                    self.data.append(post_data)
-                    c += 1
-        def export_to_csv(self):
-           
-            try:
-                try:
-                    os.rename("data/mercadolibre_data.csv", "data/mercadolibre_data_old.csv")
-                except FileExistsError:
-                    os.rename("data/mercadolibre_data_old.csv", f"data/mercadolibre_data_old_ {now}.csv")
-                    os.rename("data/mercadolibre_data.csv", "data/mercadolibre_data_old.csv")
-            except FileExistsError:
-                os.remove("data/mercadolibre_data_old.csv")
-                print("data_old elimindado")
-                os.rename("data/mercadolibre_data.csv", "data/mercadolibre_data_old.csv")
-            except FileNotFoundError:
-                print("el archivo no existe creeando.... ")            
-            # export to a csv file
-            df = pd.DataFrame(self.data)
-            df.to_csv(r"data/mercadolibre_data.csv", sep=";")
-            os.rename("data/mercadolibre_data.csv", f"data/mercadolibre_data_{Numero}")
+                # save in a dictionary
+                post_data = {
+                    "title": title,
+                    "price": price,
+                    "post link": post_link,
+                #    "image link": img_link            
+                }
+                # save the dictionaries in a list
+                self.data.append(post_data)
+                c += 1
+    def export_to_csv(self):          
+        # export to a csv file
+        df = pd.DataFrame(self.data)
+        df.to_csv(r"data/mercadolibre_data.csv", sep=";")
+        os.rename("data/mercadolibre_data.csv", f"data/mercadolibre_data_{Numero}.csv")
 
 if __name__ == "__main__":
     s = Scraper()
     s.menu()
-    s.scraping()
-    s.export_to_csv()
-    
+    while(Numero <= 4):
+        Numero = Numero + 1
+        s.scraping()
+        s.export_to_csv() 
